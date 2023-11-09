fb42701bf95dc49b640d8b81997160f13785e64a
maryam <maryam_abbasi88@yahoo.com>
2023-11-09 15:46:19 +0330

    firs project upload

---
+++ SideNav.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+	}
+	function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
